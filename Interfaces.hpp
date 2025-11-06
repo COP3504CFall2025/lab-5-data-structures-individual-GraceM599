dcb1cca48dd3853a5ccc09ba1dc1f61b895505b3
Grace Mihiral <GraceM599>
2025-11-06 14:08:49 -0500

    jhs

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -38,6 +38,6 @@ public:
     virtual const T& front() const = 0;
     virtual const T& back() const = 0;
     virtual std::size_t getSize() const noexcept = 0;
-    virtual ~DequeInterface();
+    virtual ~DequeInterface() = 0;
 };
 
